16fa00a27981cbe3f0b8ef0bac18293012f6efe3
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2022-09-03 16:13:46 +0300

    feat: add booking Entity and declare Repositories

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,18 +1,49 @@
 package ru.practicum.shareit.booking;
 
-import lombok.Data;
-import org.springframework.stereotype.Component;
-import ru.practicum.shareit.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
-@Component
+@Entity
+@Table(name = "booking")
+@Getter
+@Setter
+@ToString
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "booking_id")
     private Long id;
+
+    @Column(name = "start_date")
     private LocalDateTime start;
+
+    @Column(name = "end_date")
     private LocalDateTime end;
-    private Item item;
+
+    @Column(name = "item_id")
+    private Long itemId;
+
+    @Column(name = "booker_id")
     private Long booker;
+
+    @Column(name = "status")
     private BookingStatus status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Booking)) return false;
+        return id != null && id.equals(((Booking) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.practicum.shareit.exception.InvalidParameterException;
 import ru.practicum.shareit.exception.ItemNotFoundException;
 import ru.practicum.shareit.exception.UserNotFoundException;
