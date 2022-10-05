75e03091f6a97112c488799ea30c4dc4c45956fa
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2022-10-06 00:09:31 +0300

    feat: done with Request in gateway

--- gateway/src/main/java/ru/practicum/shareit/request/RequestController.java
+++ gateway/src/main/java/ru/practicum/shareit/request/RequestController.java
@@ -1,45 +1,43 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.client.RequestClient;
 import ru.practicum.shareit.request.entity.RequestDto;
-import ru.practicum.shareit.request.entity.RequestDtoResponse;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/requests")
 public class RequestController {
 
-    public final RequestServiceImpl requestService;
+    public final RequestClient requestClient;
 
-    @Autowired
-    public RequestController(RequestServiceImpl requestService) {
-        this.requestService = requestService;
+    public RequestController(RequestClient requestClient) {
+        this.requestClient = requestClient;
     }
 
+
     @PostMapping
-    public RequestDto createRequest(@RequestHeader("X-Sharer-User-Id") Long id,
-                                    @RequestBody RequestDto requestDto) {
-        return requestService.createRequest(id, requestDto);
+    public ResponseEntity<Object> createRequest(@RequestHeader("X-Sharer-User-Id") Long id,
+                                                @RequestBody RequestDto requestDto) {
+        return requestClient.createRequest(id, requestDto);
     }
 
     @GetMapping
-    public List<RequestDtoResponse> findAllRequestsWithResponses(@RequestHeader("X-Sharer-User-Id") Long id) {
-        return requestService.findAllRequestsWithResponses(id);
+    public ResponseEntity<Object> findAllRequestsWithResponses(@RequestHeader("X-Sharer-User-Id") Long id) {
+        return requestClient.findAllRequestsWithResponses(id);
     }
 
     @GetMapping("/{requestId}")
-    public RequestDtoResponse findRequestWithResponses(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                                       @PathVariable Long requestId) {
-        return requestService.findRequestWithResponses(userId, requestId);
+    public ResponseEntity<Object> findRequestWithResponses(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                           @PathVariable Long requestId) {
+        return requestClient.findRequestWithResponses(userId, requestId);
     }
 
     @GetMapping("/all")
-    public List<RequestDtoResponse> findAllRequestsWithPagination(@RequestHeader("X-Sharer-User-Id") Long id,
-                                                                  @RequestParam(required = false) Integer from,
-                                                                  @RequestParam(required = false) Integer size) {
-        return requestService.findAllRequestsWithPagination(id, from, size);
+    public ResponseEntity<Object> findAllRequestsWithPagination(@RequestHeader("X-Sharer-User-Id") Long id,
+                                                                @RequestParam(required = false) Integer from,
+                                                                @RequestParam(required = false) Integer size) {
+        return requestClient.findAllRequestsWithPagination(id, from, size);
     }
 
 }
--- gateway/src/main/java/ru/practicum/shareit/request/client/RequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/request/client/RequestClient.java
@@ -1,4 +1,51 @@
 package ru.practicum.shareit.request.client;
 
-public class RequestClient {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.shareit.request.entity.RequestDto;
+
+import java.util.Map;
+
+@Service
+public class RequestClient extends RequestBaseClient {
+
+    private static final String API_PREFIX = "/requests";
+
+    @Autowired
+    public RequestClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> createRequest(Long id, RequestDto requestDto) {
+        return post("", id, null, requestDto);
+    }
+
+    public ResponseEntity<Object> findAllRequestsWithResponses(Long id) {
+        return get("", id, null);
+    }
+
+    public ResponseEntity<Object> findRequestWithResponses(Long userId, Long requestId) {
+        Map<String, Object> parameters = Map.of(
+                "requestId", requestId
+        );
+        return get("/" + requestId, userId, parameters);
+    }
+
+    public ResponseEntity<Object> findAllRequestsWithPagination(Long id, Integer from, Integer size) {
+        Map<String, Object> parameters = Map.of(
+                "from", from,
+                "size", size
+        );
+        return get("/all?from={from}&size={size}", id, parameters);
+    }
 }
